5f52fa15dc0b516e198b0dbcda9a96eccd878fb9
martin18f <143570325+martin18f@users.noreply.github.com>
2026-02-25 16:08:02 +0100

    Delete craco.config.js

--- craco.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    babel: {
-      plugins: [
-        // Tento plugin odstráni volania console.log iba v produkcii
-        process.env.NODE_ENV === 'production' && 'transform-remove-console',
-      ].filter(Boolean),
-    },
-  };
